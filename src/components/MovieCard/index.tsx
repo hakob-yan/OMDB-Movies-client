85e047d9f2563b6c097fb0335179a63880ff6692
hakob-yan <devhakob@gmail.com>
2023-12-16 21:16:43 +0400

    added

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -2,7 +2,14 @@ import { IMovie } from "../../redux/features/movies/moviesSlice";
 import * as S from "./styles";
 
 function MovieCard({ title, year, imdbID, type, image }: IMovie) {
-  return <S.MoviesCardWrapper key={imdbID}>MovieCard</S.MoviesCardWrapper>;
+  console.log(title);
+  
+  return (
+    <S.MoviesCardWrapper>
+      <S.MoviesCardImage key={imdbID} backgroundImage={image} />;
+      <S.MoviesCardTitle>{title}</S.MoviesCardTitle>
+    </S.MoviesCardWrapper>
+  );
 }
 
 export default MovieCard;
--- src/components/MovieCard/styles.ts
+++ src/components/MovieCard/styles.ts
@@ -1,11 +1,22 @@
 import { styled } from "styled-components";
 
+export const MoviesCardImage = styled.div<{
+  backgroundImage: string;
+}>`
+  border-radius: 1.5rem;
+  padding: 1rem;
+  background-image: ${(props) =>
+    props.backgroundImage ? `url(${props.backgroundImage})` : "none"};
+  height: 100%;
+`;
+export const MoviesCardTitle = styled.p`
+  text-align: center;
+`;
+
 export const MoviesCardWrapper = styled.div`
   width: 20rem;
   height: 100%;
-  background-color: red;
   margin: 3.5rem;
-  border-radius: 1.5rem;
-  padding: 1rem;
-
+  color: black;
+  font-size: 1.3rem;
 `;
