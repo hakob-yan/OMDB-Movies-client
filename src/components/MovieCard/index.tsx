f1dad43fec2285ebcd5d5a18ae28c1049d501343
hakob-yan <devhakob@gmail.com>
2023-12-17 12:46:27 +0400

    aded naviage

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { IMovie } from "../../redux/features/movies/moviesSlice";
 import * as S from "./styles";
 
 function MovieCard({ title, year, imdbID, type, image }: IMovie) {
-  
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/movie/${imdbID}`);
+  };
   return (
-    <S.MoviesCardWrapper>
+    <S.MoviesCardWrapper onClick={handleClick}>
       <S.MoviesCardImage key={imdbID} src={image} />
       <S.MoviesCardTitle>{title}</S.MoviesCardTitle>
     </S.MoviesCardWrapper>
