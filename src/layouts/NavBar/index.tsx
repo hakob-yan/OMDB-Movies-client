4ea1a6cbf434dbb3c4fc212745e3625c09518a5a
hakob-yan <devhakob@gmail.com>
2023-12-17 20:07:40 +0400

    adde styles

--- src/layouts/NavBar/index.tsx
+++ src/layouts/NavBar/index.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import * as S from "./styles";
-import { Link } from "react-router-dom";
-import * as paths from "../../constants/routerPaths"
-import Logo from "../../assets/images/logo.png"
+import { useNavigate } from "react-router-dom";
+import * as paths from "../../constants/routerPaths";
+import Logo from "../../assets/images/logo.png";
+import { SEARCH } from "../../constants";
 
 function NavBar() {
-    return (
-        <S.NavBar>
-            <S.LiveMovies>
-
-                <S.LiveMoviesLogo src={Logo} />
-                <p>liveMovies</p>
-            </S.LiveMovies>
-            <Link to={paths.HOME}>Movies</Link>
-        </S.NavBar>
-    );
+  const navigate = useNavigate();
+  const handleClick = () => {
+    localStorage.removeItem(SEARCH);
+    navigate("/");
+  };
+  const handleMoviesClick = () => {
+    navigate(paths.HOME);
+  };
+  return (
+    <S.NavBar>
+      <S.LiveMovies onClick={handleClick}>
+        <S.LiveMoviesLogo src={Logo} />
+        <S.LiveMoviesTitle>liveMovies</S.LiveMoviesTitle>
+      </S.LiveMovies>
+      <S.MoviesTitle onClick={handleMoviesClick}>Movies</S.MoviesTitle>
+    </S.NavBar>
+  );
 }
 
 export default NavBar;
--- src/layouts/NavBar/styles.ts
+++ src/layouts/NavBar/styles.ts
@@ -2,9 +2,9 @@ import { styled } from "styled-components";
 
 export const NavBar = styled.nav`
   position: absolute;
+  padding: 1rem 3rem;
   display: flex;
   justify-content: space-between;
-  padding: 1rem;
   width: 100%;
   z-index: 999;
   color: white;
@@ -14,10 +14,21 @@ export const LiveMovies = styled.div`
   display: flex;
   gap: 0 1rem;
   font-weight: bold;
-  
+  cursor: pointer;
 `;
 
 export const LiveMoviesLogo = styled.img`
   width: 2rem;
   height: 2rem;
+`;
+export const LiveMoviesTitle = styled.p``;
+export const MoviesTitle = styled.p`
+  cursor: pointer;
+  text-transform: uppercase;
+  font-weight: bolder;
+  border-bottom: 5px red solid;
+  transition: all 100ms ease-out;
+  &:hover {
+    border: none;
+  }
 `;
