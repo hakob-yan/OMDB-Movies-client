d38255cb01e9e2ae36426a559dd94b5d304b072c
hakob-yan <devhakob@gmail.com>
2023-12-20 00:42:38 +0400

    finished task

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,9 +1,9 @@
 import { Component, ErrorInfo } from "react";
-import { State, Props } from "./types"
+import { State, Props } from "./types";
 
 class ErrorBoundary extends Component<Props, State> {
   public state: State = {
-    hasError: false
+    hasError: false,
   };
 
   public static getDerivedStateFromError(_: Error): State {
@@ -12,13 +12,17 @@ class ErrorBoundary extends Component<Props, State> {
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error('Error caught:', error, errorInfo);
+    console.error("Error caught:", error, errorInfo);
     this.setState({ hasError: true });
   }
 
   public render() {
     if (this.state.hasError) {
-      return <div className='error-wrapper'><h1>Something went wrong. Please try again later.</h1></div>;
+      return (
+        <div>
+          <h1>Something went wrong. Please try again later.</h1>
+        </div>
+      );
     }
 
     return this.props.children;
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-// import ErrorBoundary from './components/ErrorBoundary';
-import { Provider } from 'react-redux';
-import { GlobalStyles } from './styles/Global'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { Provider } from "react-redux";
+import { GlobalStyles } from "./styles/Global";
 
-import store from "./redux/store"
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <GlobalStyles />
-    {/* <ErrorBoundary> */}
-    <App />
-    {/* </ErrorBoundary> */}
-  </Provider>
-
+  <ErrorBoundary>
+    <Provider store={store}>
+      <GlobalStyles />
+      <App />
+    </Provider>
+  </ErrorBoundary>
 );
-
